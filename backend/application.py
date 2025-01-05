8d7a8d796ec002ff36a698f28ec7db0ec2539422
Africk-Hunter <hunterafrick@gmail.com>
2025-01-05 11:59:17 -0800

    Updated for deployment

--- backend/application.py
+++ backend/application.py
@@ -69,6 +69,10 @@ def fetch_new_word():
     conn.close()
     return response
 
+@application.route('/health')
+def health_check():
+    return 'OK', 200
+
 if __name__ == '__main__':
     """ application.run(debug=True) """
     application.run(host='0.0.0.0', port=5000)
